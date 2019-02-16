2149d65de1aa58dd54f08e8e98c53ecc5fc2c788
Poowis <60070184@kmitl.ac.th>
2019-02-17 00:49:04 +0700

    save

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -9,75 +9,92 @@ class Login extends StatefulWidget {
 
 class LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
+  TextEditingController userId = new TextEditingController();
+  TextEditingController password = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              Image.asset(
-                "resources/a.jpg",
-                height: 100,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  labelText: "User Id",
-                  hintText: "Please input your User Id",
+      body: Builder(
+        builder: (context) => Container(
+              padding: EdgeInsets.all(20),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: <Widget>[
+                    Image.asset(
+                      "resources/login.png",
+                      height: 300,
+                    ),
+                    TextFormField(
+                      controller: userId,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        labelText: "User Id",
+                        hintText: "Please input your User Id",
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "กรุณาระบุ user";
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      controller: password,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: "Password",
+                        hintText: "Please input your Password",
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "กรุณาระบุ password";
+                        }
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text("LOGIN"),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          if (userId.text != "admin" ||
+                              password.text != "admin") {
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("user or password ไม่ถูกต้อง"),
+                              ),
+                            );
+                          } else {
+                            Navigator.pushReplacementNamed(context, "/main");
+                          }
+                        } else if (userId.text.isEmpty ||
+                            password.text.isEmpty) {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("กรุณาระบุ user or password"),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text(
+                            "Register New Account",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, "/register");
+                          },
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "กรุณาระบุ user or password";
-                  }
-                },
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  labelText: "Password",
-                  hintText: "Please input your Password",
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please input your Password";
-                  } else if (value != "admin") {
-                    return "user or password ไม่ถูกต้อง";
-                  }
-                },
-              ),
-              RaisedButton(
-                child: Text("LOGIN"),
-                onPressed: () {
-                  print(_formKey.currentState.validate());
-                  if (true) {
-                    print("asd");
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text("sdfsd"),
-                    ));
-                  };
-                  // Navigator.pushNamed(context, "/main");
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  FlatButton(
-                    child: Text("Register New Account"),
-                    textColor: Colors.green,
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/register");
-                    },
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
+            ),
       ),
     );
   }
--- lib/ui/register.dart
+++ lib/ui/register.dart
@@ -9,6 +9,7 @@ class Register extends StatefulWidget {
 
 class RegisterState extends State<Register> {
   final _formKey = GlobalKey<FormState>();
+  TextEditingController email = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,69 +17,84 @@ class RegisterState extends State<Register> {
         centerTitle: true,
         title: Text("REGISTER"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: "User Id",
+      body: Builder(
+        builder: (context) => Container(
+              padding: EdgeInsets.all(20),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: email,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        labelText: "Email",
+                        hintText: "Please enter your Email",
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: "Password",
+                        hintText: "Please enter your Password",
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: "Confirm Password",
+                        hintText: "Please confirm your Password",
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                        }
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text(
+                        "CONTINUE",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.blue,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          if (email.text == "admin"){
+                            Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("user นี้มีอยู่ในระบบแล้ว"),
+                            ),
+                          );
+                          }
+                          else{
+                            Navigator.pushReplacementNamed(context, "/");
+                          }
+                        } else {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("กรุณาระบุข้อมูลให้ครบถ้วน"),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please input value";
-                  }
-                },
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.lock),
-                  hintText: "Password",
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please input value";
-                  }
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.lock),
-                  hintText: "Confirm Password",
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please input value";
-                  }
-                },
-              ),
-              RaisedButton(
-                child: Text("LOGIN"),
-                onPressed: () {
-                  _formKey.currentState.validate();
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  FlatButton(
-                    child: Text("Register New Account"),
-                    textColor: Colors.green,
-                    onPressed: () {
-                      Navigator.pushNamed(context, "register");
-                    },
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
+            ),
       ),
     );
   }
